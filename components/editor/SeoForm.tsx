80e81ae0b3bcaf1f002b4104452bb46d9df0a1f1
lawrence <lawrencemichaelmargaja@gmail.com>
2023-09-17 17:28:22 +0800

    Lesson 110 completed. Added states for SEO Form. Working on Slugify.

--- components/editor/SeoForm.tsx
+++ components/editor/SeoForm.tsx
@@ -1,4 +1,4 @@
-import {ChangeEventHandler, FC} from 'react';
+import {ChangeEventHandler, FC, useState} from 'react';
 import classnames from 'classnames';
 
 interface Props {
@@ -19,19 +19,38 @@ const Input: FC<{
                     className='absolute top-1/2 -translate-y-1/2 text-sm font-semibold text-primary-dark dark:text-primary pl-2'>
                     {label}
                 </span>
-            <input
-                type="text"
-                name={name}
-                value={value}
-                placeholder={placeholder}
-                onChange={onChange}
-                className={classnames(commonInput, 'italic pl-10')}
-            />
+                <input
+                    type="text"
+                    name={name}
+                    value={value}
+                    placeholder={placeholder}
+                    onChange={onChange}
+                    className={classnames(commonInput, 'italic pl-10')}
+                />
         </label>
     )
 }
 
 const SEOForm: FC<Props> = (props): JSX.Element => {
+
+    const [values, setValues] = useState({meta: '', slug: '', tags: ''});
+
+    const handleChange: ChangeEventHandler<HTMLInputElement | HTMLInputElement> = ({target}) => {
+        let {name, value} = target
+        /**
+         * The commented out code is below and it works but I decided to use the code from the tutorial.
+         * The reason is because my code works for stoping at 150 characters but you cannot remove added values.
+         */
+        // if(name === 'meta' && meta.length === 150) {
+        //     return
+        // }
+        if(name === 'meta') value = value.substring(0, 150);
+        console.log("the target.name === ", target.name);
+        setValues({...values, [name]: value})
+    }
+
+    const {meta, slug, tags} = values;
+
     return (
         <div className="space-y-4">
             <h1 className="text-primary-dark dark:text-primary text-xl font-semibold">
@@ -42,16 +61,28 @@ const SEOForm: FC<Props> = (props): JSX.Element => {
                 name="slug"
                 placeholder="slug-goes-here"
                 label="Slug:"
+                value={slug}
+                onChange={handleChange}
             />
             <Input
                 name="tags"
                 placeholder="React, Next JS"
                 label="Tags:"
+                value={tags}
+                onChange={handleChange}
             />
+            <div className="relative">
+                <textarea
+                    name="meta"
+                    value={meta}
+                    className={classnames(commonInput, 'text-lg h-20 resize-none')}
+                    placeholder="meta description"
+                    onChange={handleChange}
+                >
 
-            <textarea className={classnames(commonInput, 'text-lg h-20 resize-none')} placeholder="meta description">
-
-            </textarea>
+                </textarea>
+                <p>{meta.length}/150</p>
+            </div>
         </div>
     )
 
