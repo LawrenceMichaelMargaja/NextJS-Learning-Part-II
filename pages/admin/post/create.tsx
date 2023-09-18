847117893d7b7defac02431feb4b1bb070f9f28d
lawrence <lawrencemichaelmargaja@gmail.com>
2023-09-19 00:32:59 +0800

    Lesson 123 completed. Accepting of FormData. Solved issue for createNewPost error. Added Utils.

--- pages/admin/post/create.tsx
+++ pages/admin/post/create.tsx
@@ -1,17 +1,62 @@
 import Editor from '../../../components/editor';
 import { NextPage } from 'next';
 import AdminLayout from "../../../components/layout/AdminLayout";
+import {FinalPost} from "../../../components/editor/index";
+import axios from "axios";
 
 interface Props {}
 
 const Create: NextPage<Props> = () => {
+
+    // const handleSubmit = async (post: FinalPost) => {
+    //
+    //     try {
+    //         // we have to generate FormData
+    //         const formData = new FormData()
+    //         for(let key in post) {
+    //             const value = (post as any)[key]
+    //             if(key === 'tags') {
+    //                 const tags = value.split(',').map((tag: string) => tag.trim());
+    //                 formData.append('tags', JSON.stringify(tags));
+    //             } else {
+    //                 formData.append(key, value);
+    //             }
+    //         };
+    //
+    //         // submit our post
+    //         const { data } = await axios.post('/api/posts');
+    //         console.log("the data from submit post === ", data);
+    //     } catch (error) {
+    //         console.log("the error in the handle submit --- ", error.response.data)
+    //     }
+    // };
+
+    const handleSubmit = async (post: FinalPost) => {
+        try {
+            // we have to generate FormData
+            const formData = new FormData();
+            for (let key in post) {
+                const value = (post as any)[key];
+                if (key === "tags" && value.trim()) {
+                    const tags = value.split(",").map((tag: string) => tag.trim());
+                    formData.append("tags", JSON.stringify(tags));
+                } else formData.append(key, value);
+            }
+
+            // submit our post
+            const { data } = await axios.post("/api/posts", formData);
+
+            console.log(data);
+        } catch (error: any) {
+            console.log(error.response.data);
+        }
+    };
+
     return (
         <AdminLayout title='New Post'>
             <div className="max-w-4xl mx-auto">
                 <Editor
-                    onSubmit={(post) => {
-                        console.log("the post ---- ", post);
-                    }}
+                    onSubmit={handleSubmit}
                 />
             </div>
         </AdminLayout>
