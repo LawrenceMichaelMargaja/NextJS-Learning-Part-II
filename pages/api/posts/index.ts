2ab1f9e5a11ed25c6042e75a911c2f0832fd824b
lawrence <lawrencemichaelmargaja@gmail.com>
2023-09-18 22:02:51 +0800

    Post validation added.

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -1,5 +1,7 @@
 import {NextApiHandler} from "next";
 import dbConnect from "../../../lib/dbConnect";
+import Joi from "joi";
+import {postValidationSchema, validateSchema} from "../../../lib/validator";
 
 const handler: NextApiHandler = async (req, res) => {
     const {method} = req
@@ -9,7 +11,21 @@ const handler: NextApiHandler = async (req, res) => {
             await dbConnect();
             res.json({ ok: true });
         }
+        case 'POST': {
+            return createNewPost(req, res)
+        }
     }
 };
+
+const createNewPost: NextApiHandler = (req, res) => {
+    const {body} = req;
+    const error  = validateSchema(postValidationSchema, body);
+
+    if(error) {
+        return res.status(400).json({ error })
+    }
+
+    res.json({ok: true});
+}
 
 export default handler;
